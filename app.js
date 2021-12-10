7fd7b645e99ad2b103084f0e35b5c0e02ee807da
flotr18 <55090943+flotr18@users.noreply.github.com>
2021-12-10 11:34:18 +0100

    Added MongoDB Database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { MongoClient } = require('mongodb')
+const uri = "mongodb+srv://admin:root@cluster0.elh7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(express.static('public'));
 
@@ -11,4 +13,14 @@ app.get('/', function(req, res){
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+MongoClient.connect(uri, { useUnifiedTopology: true },function(err, db) {
+  if (err) throw err;
+  if ( !err){
+    console.log('It Works !')
+  }
+});
+
+
+
