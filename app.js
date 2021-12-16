2ce83bf406ccfd09e1fb7fd3d2cceb20a86efbbf
flotr18 <55090943+flotr18@users.noreply.github.com>
2021-12-16 20:12:35 +0100

    Added Vue

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-
-
+const bodyParser = require("body-parser");
 
 const { MongoClient } = require('mongodb')
 const uri = "mongodb+srv://admin:root@cluster0.elh7n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
@@ -17,40 +15,33 @@ app.get('/register', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/inscription.html'));
 });
 
-app.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/connexion.html'));
-});
-
-app.get('/afterwork', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/afterwork.html'));
-});
-
-app.get('/poll', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/poll.html'));
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-app.use
+
 
 
 MongoClient.connect(uri, { useUnifiedTopology: true },function(err, db) {
   if (err) throw err;
   let dbo = db.db("devlab15");
 
-
   if ( !err){
     console.log('It Works !')
   }
 
+  app.use(bodyParser.urlencoded({extended : false }));
+  app.use(bodyParser.json());
+
+
+
   dbo.collection("event").find({}).toArray(function(err, res) {
     if (err) throw err;
     console.log(res);
-    db.close()
 
   });
+
+
 
 
 
