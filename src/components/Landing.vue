61ab433ecd2f1522d8a0fdd16e37aceedee0bc22
flotr18 <55090943+flotr18@users.noreply.github.com>
2022-01-16 00:48:04 +0100

    Event on Landing V0

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -5,9 +5,32 @@
     <h2>your email is: {{ email }}</h2>
 
 
-    <ul v-for="item in eventname" :key="item.eventid">
-      {{ item.name }}
-    </ul>
+    <div class="grid-container">
+
+      <v-card v-for="item in eventname" :key="item.eventid"
+       min-height="300"
+       max-width="200"
+       class=""
+       elevation="2"
+      >
+        <v-card-title>{{ item.name }}</v-card-title>
+        <v-card-subtitle>01/01/1970 - {{ item.attendance }}</v-card-subtitle>
+        <v-card-text>Does everybody know that pig named Lorem Ipsum ? Some people have an ability to write placeholder text…</v-card-text>
+
+
+
+      </v-card>
+
+
+
+
+
+
+    </div>
+
+
+
+
 
 
 
@@ -55,3 +78,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  justify-content: center;
+  gap: 30px;
+  column-gap: 30px;
+
+}
+</style>
